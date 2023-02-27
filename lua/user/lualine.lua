d8f2727d3d35c9ef654641bd39c4e1180442c0c6
Farrukh Kamal <farrukhkamal429@gmail.com>
2023-02-27 18:30:05 +0500

    lualine theme switched to onedark

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -43,7 +43,7 @@ lualine.setup({
 	options = {
 		globalstatus = true,
 		icons_enabled = true,
-		theme = "ayu",
+		theme = "onedark",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "alpha", "dashboard" },
